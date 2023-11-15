045107ff3e00f46b86fbd0f47ad063a7fb9b4a10
ana-m-gaina <ana.m.gaina@gmail.com>
2023-11-15 12:26:42 +0200

    update requirements

--- homeinsightprj/settings.py
+++ homeinsightprj/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1(cw0_y*5(a8#zm0ip#k1hf2kc3+u)v2w^zc&e3(3gw3s1ba5i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 
